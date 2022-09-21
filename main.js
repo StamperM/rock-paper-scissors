7326dddfd99a42cbd63d87ed6e870ebf226de4a0
Melissa Gillen <melissagillen3@gmail.com>
2022-09-20 20:36:08 -0400

    made changes to playRound

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
 const choice = ['Rock', 'Paper','Scissor'];
 
 function getComputerChoice(){
-   let computerSelection = (Math.floor(Math.random() * choice.length));
-   if (computerSelection=== 0){
+   let computerChoiceInput = (Math.floor(Math.random() * choice.length));
+   if (computerChoiceInput === 0){
     return "rock"
-}else if (computerSelection === 1 ){
+}else if (computerChoiceInput === 1 ){
     return 'paper'
-}else(computerSelection === 2)
+}else(computerChoiceInput  === 2)
     return 'scissors'
 
 }
+function playerSelection(){
+    playerInput = prompt('Choose your weapon! Rock, Paper or Scissor')
+    return playerInput.toLowerCase()
 
-function singleRound(playerSelection,getComputerChoice){
-    if (getComputerChoice === 'rock'&& playerSelection === 'paper')
+    function playRound(computerSelection,playerSelection){
+    if (computerSelection === 'rock'&& playerSelection === 'paper')
         return ('You win! Paper beats rock')
-        else if (getComputerChoice ===1 && playerSelection === 'scissor')
+        else if (computerSelection ===1 && playerSelection === 'scissor')
         return('Better luck next time. Rock Beats Scissor.')
-        else if (getComputerChoice === 1 && playerSelection === 'rock')
+        else if (computerSelection === 1 && playerSelection === 'rock')
         return ('It"s a draw try again.')
-        if (getComputerChoice === 'paper' && playerSelection === 'paper')
+        if (computerSelection === 'paper' && playerSelection === 'paper')
         return ('It"s a draw try again.')
-        else if (getComputerChoice === 'paper' && playerSelection === 'scissor')
+        else if (computerSelection === 'paper' && playerSelection === 'scissor')
         return('You win! Scissor beats Paper.')
-        else if (getComputerChoice === 'paper' && playerSelection === 'rock')
+        else if (computerSelection === 'paper' && playerSelection === 'rock')
         return ('Sorry, you lose. Paper Beats Rock')
-        else if ( getComputerChoice === 'scissor' && playerSelection === 'paper')
+        else if ( computerSelection === 'scissor' && playerSelection === 'paper')
         return('Sorry you lose. Paper loses to scissor.')
-        else if (getComputerChoice === 'scissor' && playerSelection === 'rock')
+        else if (computerSelection === 'scissor' && playerSelection === 'rock')
         return('You win! Rock beats scissor')
-        else (getComputerChoice === ' scissor' && playerSelection === 'scissor')
+        else (computerSelection=== ' scissor' && playerSelection === 'scissor')
         return ('No, winner. Its a draw.')
 }
 
+}
 
-// function roundPlay () {
-//     
-// }
-// function roundPlay () {
-//    
+
+
